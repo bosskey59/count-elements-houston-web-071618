fdd05dac0b64bd4fcc5de9a21bdaf75afec694cc
Alejandro Aguilar Jr <alejandro.aguilar.jr@gmail.com>
2018-07-17 00:36:18 -0500

    Done

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,15 @@
+require "pry"
 def count_elements(array)
   # code goes here
+  new_hash = {}
+  # binding.pry
+  array.each do |index|
+    if !new_hash.keys.include?(index)
+      new_hash[index]= 1
+    else
+      new_hash[index] += 1
+    end
+  end
+
+  new_hash
 end
- 
